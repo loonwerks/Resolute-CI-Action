7505ed0492ae221893fd8adc25d4224a523e4b22
Karl Hoech <karl.hoech@collins.com>
2026-01-13 10:28:15 -0600

    Add test script

---
+++ test-container.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+set -Eeuxo pipefail
+
+: "${workspace_location:=.}"
+: "${component_to_analyze:=platform::ZCU102.Impl}"
+: "${project_path:=}"
+: "${output_path:=./Resolute_output.json}"
+: "${validation_only:=}"
+: "${csv_output:=}"
+: "${exit_on_warning:=}"
+: "${supplementary_aadl:=}"
+: "${GITHUB_WORKSPACE:=/home/runner/work}"
+
+docker run --rm -v $1:${GITHUB_WORKSPACE} \
+    -v ./:/home/runner \
+    -e GITHUB_WORKSPACE=${GITHUB_WORKSPACE} \
+    -e GITHUB_OUTPUT='/dev/stdout' \
+    --entrypoint /home/runner/entrypoint.sh \
+    ghcr.io/loonwerks/fmide-tools:1.0.0 \
+    "${workspace_location}" "${component_to_analyze}" "${project_path}" "${output_path}" \
+    "${validation_only}" "${csv_output}" "${exit_on_warning}" "${supplementary_aadl}"
+
