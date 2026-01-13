68f79e9e0bf13829a4df58d0cbbf9b985b054c01
Karl Hoech <karl.hoech@collins.com>
2026-01-13 10:27:30 -0600

    Update entrypoint parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,4 @@
-#!/bin/sh -l
-
-resoluteOutput=$GITHUB_WORKSPACE/Resolute_output.json
-ls -la $GITHUB_WORKSPACE
+#!/bin/bash -l
 
 echo "workspace-location: $1"
 echo "component-to-analyze: $2"
@@ -11,44 +8,44 @@ echo "validation-only: $5"
 echo "csv-output: $6"
 echo "exit-on-warning: $7"
 echo "supplementary-aadl: $8"
-echo "resoluteOutput: $resoluteOutput"
 
-runCommand = (/fmide/osate -application com.rockwellcollins.atc.resolute.cli.Resolute \
-  -noSplash -data $GITHUB_WORKSPACE/$1 -compImpl $2 -output $4)
+runCommand=(/fmide/osate -application com.rockwellcollins.atc.resolute.cli.Resolute)
+
+runCommand+=(-noSplash -data ${GITHUB_WORKSPACE}/$1 -compImpl $2)
 
 if [[ -n $3 ]]; then
-	runCommand += (-p $3)
+	runCommand+=(-p $3)
 fi
 
 if [ "XX $5" = 'XX "true"' ] ; then
-	runCommand += (-validationOnly)
+	runCommand+=(-validationOnly)
 fi
 
 if [ "XX $6" = 'XX "true"' ]; then
-	runCommand += (-csv)
+	runCommand+=(-csv)
 fi
 
 if [ "XX $7" = 'XX "true"' ]; then
-	runCommand += (-exitOnValidationWarning)
+	runCommand+=(-exitOnValidationWarning)
 fi
 
 if [[ -n $8 ]]; then
-	runCommand += (-files $8)
+	runCommand+=(-files $8)
 fi
 
-runCommand += (-o $resoluteOutput $resoluteOutput)
+runCommand+=(-o $4)
 
 xvfb-run -e /dev/stdout -s "-screen 0 1280x1024x24 -ac -nolisten tcp -nolisten unix" "${runCommand[@]}"
 
-echo "timestamp=$(jq .date $resoluteOutput)" >> $GITHUB_OUTPUT
-echo "status=$(jq .status $resoluteOutput)" >> $GITHUB_OUTPUT
-echo "status-messages=$(jq .statusMessages $resoluteOutput)" >> $GITHUB_OUTPUT
+echo "timestamp=$(jq .date $4)" >> $GITHUB_OUTPUT
+echo "status=$(jq .status $4)" >> $GITHUB_OUTPUT
+echo "status-messages=$(jq .statusMessages $4)" >> $GITHUB_OUTPUT
 
 exitStatus=1
-analysisStatus=$(jq .status $resoluteOutput)
+analysisStatus=$(jq .status $4)
 echo "analysisStatus: $analysisStatus"
 if [ "XX $analysisStatus" = 'XX "Analysis Completed"' ]; then
-	claimsTrue=$(jq "[.results[] | .status] | all" $resoluteOutput)
+	claimsTrue=$(jq "[.results[] | .status] | all" $4)
 	if [ "XX $claimsTrue" = 'XX "true"' ]; then
 		exitStatus=0
 	fi
